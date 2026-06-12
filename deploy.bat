64924460e53fd83910dc983dceb797123f0ad2e7
celsojr-websters <celso.j@websters.ae>
2026-06-12 11:18:04 +0400

    Update

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,8 @@
 cd /d C:\inetpub\wwwroot\websters.ae\httpdocs\admin-panel-node
 
 echo Pulling latest code...
-git pull websters main
+git pull origin main --no-edit
+git push websters main
 
 echo Installing dependencies...
 call npm i --no-audit --no-fund
